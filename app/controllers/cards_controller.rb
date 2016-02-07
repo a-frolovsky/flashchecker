a138a4a8445984f1547f72bd12537e978412490f
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-07 21:05:01 +0400

    New, Edit, Delete, Simple_form, styles

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,42 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update_attributes(card_params)
+      redirect_to cards_path
+    else
+      redirect_to edit_card_path(@card)
+    end
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.create(card_params)
+
+    if @card.valid?
+      redirect_to cards_path
+    else
+      redirect_to new_card_path
+    end
+  end
+
+  def destroy
+    Card.find(params[:id]).destroy
+    redirect_to cards_path
+  end
+
+  private
+  def card_params
+    params.require(:card).permit!
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,10 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  before_validation :original_not_equal_translated
+
+  private
+  def original_not_equal_translated
+    return false if original_text.downcase == translated_text.downcase
+  end
 end
