048d8ef9ef2ec6730f724c403ffd8faa5b58c4f0
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-08 21:25:23 +0400

    Nokogiri in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
-Card.create([
-  {
-    original_text: 'Hello',
-    translated_text: 'Привет',
-    review_date: '2016-02-07'
-  },
-  {
-    original_text: 'Good',
-    translated_text: 'Хорошо',
-    review_date: '2016-02-07'
-  },
-  {
-    original_text: 'Win',
-    translated_text: 'Победа',
-    review_date: '2016-02-07'
-  }
-])
+require 'open-uri'
+require 'nokogiri'
+
+url = "http://sloovo.com/ru/biblioteka.php?type=obuchenie&language=EN&category=statja&url=samye-populyarnye-angliyskie-slova"
+document = Nokogiri::HTML(open(url))
+words = document.xpath('//*[@id="content"]/div/div[3]/div[2]/table[1]/tbody/*/td')
+
+words.each do |w|
+  # original - transpated
+  item = w.text.split('-')
+
+  puts "#{w.text} loaded"
+
+  Card.create({
+    original_text: item[0].to_s.strip.capitalize,
+    translated_text: item[1].to_s.mb_chars.strip.capitalize,
+    review_date: Time.now + 3.days
+  })
+end
+
+puts 'Complete.'
