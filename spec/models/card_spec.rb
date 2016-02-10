19272fbd10707419ef07b0132e547a74fdfb5e1f
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-10 19:02:12 +0400

    Add test for card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  let(:card) { create :card, original_text: "House", translated_text: "Дом" }
+  let(:card) { create :card, original_text: "House", translated_text: "Дом", review_date: Time.now }
 
   describe '#check_answer' do
     context 'when right answer' do
       it {
         expect(card.check_answer 'HoUsE').to be true
+      }
+    end
+
+    context 'review_date updated' do
+      it {
+        if card.check_answer 'HoUsE'
+          expect(card.review_date.to_s).to eq((Time.now + 3.day).strftime("%Y-%m-%d"))
+        end
       }
     end
 
