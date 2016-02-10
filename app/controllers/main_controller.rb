d77456574d2860df76779e36bd24d4c49f79a540
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-11 03:30:31 +0400

    WTF

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
-    @revision_card = Card.revision.random.take
+    # @revision_card = Card.revision.random.take
+    @revision_card = Card.order("RANDOM()").first
   end
 end
