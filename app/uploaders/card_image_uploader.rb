170e888fedf1d56fe80c5cdf7448f546630caf37
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-19 00:00:54 +0400

    Test for uploader

--- app/uploaders/card_image_uploader.rb
+++ app/uploaders/card_image_uploader.rb
@@ -7,8 +7,11 @@ class CardImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -48,5 +51,4 @@ class CardImageUploader < CarrierWave::Uploader::Base
   def filename
     "#{model.id}_#{original_filename}" if original_filename
   end
-
 end
--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     original_text "House"
     translated_text "ДоМ"
     review_date Time.now
+    # card_image
   end
 end
