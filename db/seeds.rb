d71026afa02f93bb93c1e235c0b7c9b090e093d2
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-19 21:32:36 +0400

    Comment all seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-require 'open-uri'
-require 'nokogiri'
-
-url = "http://sloovo.com/ru/biblioteka.php?type=obuchenie&language=EN&category=statja&url=samye-populyarnye-angliyskie-slova"
-document = Nokogiri::HTML(open(url))
-words = document.xpath('//*[@id="content"]/div/div[3]/div[2]/table[1]/tbody/*/td')
-
-words.each do |w|
-  # original - translated
-  item = w.text.split('-')
-  Card.create({
-    original_text: item[0].to_s.strip.capitalize,
-    translated_text: item[1].to_s.mb_chars.strip.capitalize,
-    review_date: Time.now
-  })
-end
-puts 'Cards loaded...'
+# require 'open-uri'
+# require 'nokogiri'
+#
+# url = "http://sloovo.com/ru/biblioteka.php?type=obuchenie&language=EN&category=statja&url=samye-populyarnye-angliyskie-slova"
+# document = Nokogiri::HTML(open(url))
+# words = document.xpath('//*[@id="content"]/div/div[3]/div[2]/table[1]/tbody/*/td')
+#
+# words.each do |w|
+#   # original - translated
+#   item = w.text.split('-')
+#   Card.create({
+#     original_text: item[0].to_s.strip.capitalize,
+#     translated_text: item[1].to_s.mb_chars.strip.capitalize,
+#     review_date: Time.now
+#   })
+# end
+# puts 'Cards loaded...'
