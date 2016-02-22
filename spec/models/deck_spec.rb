8d625250b2e943636bb89a0d35f14bb96f7b4cd2
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-22 20:43:01 +0400

    Deck

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Deck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create :user }
+  let(:deck) { create :deck, user: user }
+
+  it 'current status = false to default' do
+    expect(deck.current).to be false
+  end
+
+  describe '123' do
+    let!(:user) { create :user }
+
+    it 'only one deck is current' do
+      create(:deck, title: 'first', user: user)
+      create(:deck, title: 'second', user: user)
+
+      user.decks.first.update_attribute(:current, true)
+      user.decks.second.update_attribute(:current, true)
+
+      expect(user.decks.first.current).to be false
+    end
+  end
 end
