f93ee2ec8078676d18f03f02a9a4239ad6abd1e4
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-02-29 23:14:27 +0400

    remove defaults

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "sandbox68fabb31b2e0469b9c4c1cc3f9313366.mailgun.org"
   layout 'mailer'
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationMailer < ApplicationMailer
   def pending_cards(user)
     @user = user
-    mail to: @user.email, subject: "Карточки на проверку!"
+    mail from: 'sandbox68fabb31b2e0469b9c4c1cc3f9313366.mailgun.org', to: @user.email, subject: "Карточки на проверку!"
   end
 end
