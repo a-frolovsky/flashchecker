b4f38fedf58e5b6c853364b44e2247ab396b0f29
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-03-02 20:04:53 +0400

    Change local from menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
   include HttpAcceptLanguage::AutoLocale
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_login
+  before_action :require_login, :set_locale
 
   private
   def not_authenticated
     redirect_to login_path
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,7 @@ module Flashcheker
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :en
-
+    config.i18n.default_locale = :en
     config.i18n.available_locales = %w(en ru)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
